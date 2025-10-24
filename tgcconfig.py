dbd2cc49890f51a5e58a04faa321491c4f8df8f2
anyoak <anyoakmail@gmail.com>
2025-10-24 08:41:24 +0300

    Create tgcconfig.py

---
+++ tgcconfig.py
@@ -0,0 +1,6 @@
+API_ID = 24168799  # Replace with your Telegram API ID
+API_HASH = "77228f50ec27e0cc6faf5aaf25bedfd4"
+BOT_TOKEN = "8272700162:AAHbAoRp_egv4I3kz6xVqJeuSgWIbOLtoxw"
+
+SUPPORT_USERNAME = "@professor_cry"
+REQUIRED_CHANNEL = "@mailtwist"  # Channel username without 'https://t.me/'
