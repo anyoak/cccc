2e2788b05715a89daf823acc4f5603bfebfcfe63
anyoak <anyoakmail@gmail.com>
2025-12-05 21:08:45 +0200

    Create iiconfig.py

---
+++ iiconfig.py
@@ -0,0 +1,10 @@
+# config.py
+BOT_TOKEN = "7744467305:AAElMd8DRrtvDcHq9HQvtMTs3G8pO51lsRM"
+CHAT_ID = -1002360271317
+LOGIN_URL = "https://www.ivasms.com/login"
+SMS_URL = "https://www.ivasms.com/portal/live/my_sms"
+BASE_URL = "https://www.ivasms.com/portal"
+DOWNLOAD_FOLDER = "downloads"
+MAX_ERRORS = 5
+CHECK_INTERVAL = 3
+LOGIN_TIMEOUT = 300
